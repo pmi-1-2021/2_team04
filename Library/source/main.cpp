11c3b19de311c9ec509a62466016a7e77bd53475
Soerful <andriy.ishchukk@gmail.com>
2022-05-28 22:26:06 +0300

    Pre-merge commit, main.cpp is up to merged branch

--- Library/source/main.cpp
+++ Library/source/main.cpp
@@ -1,11 +1,39 @@
 #include "menu.h"
+#include "global_transactions.h"
+#include "user_transactions.h"
+#include "book_transactions.h"
+#include "bookstaking.h"
 
+
+#include <vector>
 
 
 
 int main()
 {
-	amilib::Menu m;
-	m.callMain();
+	amilib::BooksDataBase* booksBase = new amilib::BooksDataBase("..\\BusinessData\\booksInfo.txt");
+	amilib::UsersDataBase* usersBase = new amilib::UsersDataBase("..\\BusinessData\\users.txt");
+
+	amilib::IdCreator* bookIdCreator = new amilib::BooksDataBase("..\\BusinessData\\booksInfo.txt");
+	amilib::IdCreator* userIdCreator = new amilib::UsersDataBase("..\\BusinessData\\users.txt");
+
+	amilib::AddBookTransaction::ids = bookIdCreator;
+	amilib::AddUserTransaction::ids = userIdCreator;
+
+	amilib::GlobalFileTransactionsClient* booksBaseTclient = new amilib::BooksDataBase("..\\BusinessData\\booksInfo.txt");
+	amilib::GlobalFileTransactionsClient* usersBaseTclient = new amilib::UsersDataBase("..\\BusinessData\\users.txt");
+	amilib::GlobalFileTransactionsClient* takeReturnBaseTclient = new amilib::BooksTakingBase("..\\BusinessData\\takings.txt");
+
+	std::vector<amilib::GlobalFileTransactionsClient*> transactionClients;
+	transactionClients.push_back(takeReturnBaseTclient);
+	transactionClients.push_back(booksBaseTclient);
+	transactionClients.push_back(usersBaseTclient);
+	amilib::CreateTransaction::clients = transactionClients;
+
+	amilib::GlobalFileTransactionsClient* m = new amilib::Library(*booksBase, *usersBase);
+	amilib::CreateTransaction::addClient(m);
+
+	amilib::Library* menu = dynamic_cast<amilib::Library*>(m);
+	menu->callMenu();
 	return 0;
 }
